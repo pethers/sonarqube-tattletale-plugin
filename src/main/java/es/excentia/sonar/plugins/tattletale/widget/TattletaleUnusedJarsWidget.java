26abe1749b5bcddf18aa786996ccea7bfadb9dc9
jbadenas <jbadenas@excentia.es>
2014-06-17 16:39:11 +0200

    Cabecera actualizada

--- src/main/java/es/excentia/sonar/plugins/tattletale/widget/TattletaleUnusedJarsWidget.java
+++ src/main/java/es/excentia/sonar/plugins/tattletale/widget/TattletaleUnusedJarsWidget.java
@@ -1,6 +1,6 @@
 /*
  * Sonar Tattletale Plugin
- * Copyright (C) 2012 eXcentia
+ * Copyright (C) 2012 excentia
  * contact@excentia.es
  *
  * This program is free software; you can redistribute it and/or
